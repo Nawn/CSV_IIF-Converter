1d78e432817e58d8826abbf8dc93989f00d31dfb
Raul A. Gonzalez <Raul A. Gonzalez>
2016-09-22 15:55:18 -0700

    Write comments

--- convert.rb
+++ convert.rb
@@ -7,12 +7,15 @@ bot_header = %w(!ENDTRNS)
 
 headers = [top_header, mid_header, bot_header]
 
+#Declare array to store CSV rows
 file_contents = []
+#For each line in the CSV File
 CSV.foreach("EXPORT.csv") do |row|
-	file_contents << row
+	file_contents << row #Add the row of data as an Array to the file_contents array
 end
 
 File.open("Example.iif", "w") { |io|
+	#Add the header definitions at the top of the IIF.
 	headers.each do |header|
 		io.write(header.join("\t"))
 		io.write("\n")
