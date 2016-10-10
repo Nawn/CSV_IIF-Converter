c8c47060ad3a2d685095de8c8c0806e34c2986f7
Raul A. Gonzalez <Raul A. Gonzalez>
2016-10-10 09:45:30 -0700

    Refactor code

--- convert.rb
+++ convert.rb
@@ -1,44 +1,65 @@
-require_relative 'converter.rb'
 require_relative 'template.rb'
+require_relative 'converter.rb'
 
+#To Raise later if the REVISE File exists
 class FileExistContinue < StandardError
 end
 
+#Declare Empty Array
 options = []
 
+
+#For each Constant (aka class) add an instance of it to options
+#It will display the options later. Skip first because that's
+#The default Template class
 Templates.constants.each_with_index do |constant, idx|
 	options << Templates.const_get(constant).new unless idx==0
 end
 
+#Declaration
 converter = Converter.new
 
 puts "Thank you for using the CSV to IIF file converter"
 puts "\n\n\n"
 
+#Checks to see if required folders exist, if not make them
 converter.folders()
 
 begin
 	puts "Please select import template: \n\n"
 
+	#Display each option to stdout
 	options.each_with_index do |option, idx|
 		puts "#{idx+1}: #{option.display_name}"
 	end
 
 	response = gets.chomp
+	#Exception Handling
 	raise ArgumentError.new("\n\nInput must be a number\n\n") unless response =~ /\d+/
 	raise ArgumentError.new("\n\nInput must be positive number\n\n") if response.to_i < 1
 	
+	#If it passes the ArgumentErrors, It is a valid input
+	#Therefore, if it's valid, make the template whichever space
+	#They selected. (Gets rid of a case statement. Saves like6 lines of code)
 	template = options[response.to_i - 1]
+
+	#Just in case ;)
 	raise ArgumentError.new("\n\nThat number does not have a Template assigned\n\n") if template.nil?
 	
+	#If they've already begun work, raise this. 
   raise FileExistContinue.new() if File.exists?("#{converter.export_folder}/REVISE.csv")
   
+  #Reviewing the steps with the user
 	puts "Please ensure that your CSV file is: \n1.) in the #{converter.import_folder} folder\n2.) titled EXPORT.csv\n3.) Following format: #{template.desc}\n4.) Ensure your comparison file is in the \'config\' folder, titled: #{template.config_name}"
 	sleep 1
+	#This will send Windows a command to open the folder that is 
+	#supposed to contain the EXPORT.csv file
 	system ( "start #{converter.import_folder}" )
 	puts "Press enter when you're ready.."
 	gets
 	raise ArgumentError.new("CSV FILE NOT IN DIRECTORY!\n\n") unless File.exists?("#{converter.import_folder}/EXPORT.csv")
+
+
 rescue ArgumentError => e
 	puts "#{e}"
 	retry
@@ -49,12 +70,7 @@ rescue FileExistContinue => e
 
   case user_response.chomp.downcase
   when 'y'
-	  #Grab the REVISE.csv file.
-		filtered = converter.grab_trans(:revise)
-		#Remove the headers that I added to help User know what he's editing Revise.
-		filtered.shift
-		#Turn the filtered array into an array that becomes IIF when tab-delim
-		converter.convert(filtered, template)
+  	converter.convert(template)
 		exit
 	when 'n'
 		File.delete("#{converter.export_folder}/REVISE.csv")
@@ -64,50 +80,13 @@ rescue FileExistContinue => e
 	end
 end
 
-raw_csv = converter.grab_trans
-
-rejected = raw_csv.reject do |row|
-	template.valid_row?(row)
-end
-
-repeat = true
-
-display_reject = rejected.map {|row| row << "\n"}.join(", ")
-
-puts "These are the rows that the program will filter out:\n\n#{display_reject}\n\n"
-
-puts "Would you like to continue? [y/n]"
-
-Templates::Template.yn_continue("Please edit your CSV sheet to match the requirements")
-
-desired_rows = raw_csv.find_all {|row| template.valid_row?(row)}
-
-#Filtered will go through the list and apply the rules that we set up(This description math = This name/accnt)
-filtered = template.filter(desired_rows)
-#This adds header to the CSV so that Alex can know what he's editing and where to edit it.
-filtered.unshift(template.temp_header)
-
-
-#Prints out a Revise
-puts "Generating REVISE file in #{converter.export_folder}/..."
-
-
-CSV.open("#{converter.export_folder}/REVISE.csv", "w") do |csv|
-	filtered.each do |row|
-		csv << row
-	end
-end
+converter.gen_revise(template)
 
 puts "There is a new file titled REVISE.csv in the #{converter.export_folder} folder"
 puts "Please open, and make any changes before we continue. Press enter when you are done."
 system ( "start #{converter.export_folder}" )
 gets.chomp
 
-#Grab the REVISE.csv file.
-filtered = converter.grab_trans(:revise)
-#Remove the headers that I added to help User know what he's editing Revise.
-filtered.shift
-#Turn the filtered array into an array that becomes IIF when tab-delim
-converter.convert(filtered, template)
+converter.convert(template)
 
-gets.chomp
+puts "Complete...\n Thank you!"
--- converter.rb
+++ converter.rb
@@ -44,9 +44,47 @@ class Converter
 		file_contents
 	end
 
-	def convert(input_array, input_template)
+	def gen_revise(template)
+		raw_csv = grab_trans
+
+		rejected = raw_csv.reject do |row|
+			template.valid_row?(row)
+		end
+
+		display_reject = rejected.map {|row| row << "\n"}.join(", ")
+
+		puts "These are the rows that the program will filter out:\n\n#{display_reject}\n\n"
+
+		puts "Would you like to continue? [y/n]"
+
+		Templates::Template.yn_continue("Please edit your CSV sheet to match the requirements")
+
+		desired_rows = raw_csv.find_all {|row| template.valid_row?(row)}
+
+		#Filtered will go through the list and apply the rules that we set up(This description math = This name/accnt)
+		filtered = template.filter(desired_rows)
+		#This adds header to the CSV so that Alex can know what he's editing and where to edit it.
+		filtered.unshift(template.temp_header)
+
+
+		#Prints out a Revise
+		puts "Generating REVISE file in #{@export_folder}/..."
+
+
+		CSV.open("#{@export_folder}/REVISE.csv", "w") do |csv|
+			filtered.each do |row|
+				csv << row
+			end
+		end
+	end
+
+	def convert(input_template)
+		to_go = grab_trans(:revise)
+		to_go.shift
+
+
 		File.open("#{@export_folder}/COMPLETE.iif", "w") do |io|
-			input_template.generate(input_array).each do |row|
+			input_template.generate(to_go).each do |row|
 				io.write( "#{row.join("\t")}\n" )
 			end
 		end
